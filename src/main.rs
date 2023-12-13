1020bfba6b031f6a445da340e0939f18a4fbc2bc
Julian Huarachi <julian.huarachi@onesolutionsweb.com>
2023-12-13 09:45:33 -0400

    debug

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    // Generate a random number between 1 and 100
+    let mut rng = rand::thread_rng();
+    let random_number = rng.gen_range(1..=100);
+
+    println!("Random number: {}", random_number);
 }
