084fba68359c07e639e3c5c29153a6c52d294ae3
Julian Huarachi <julian.huarachi@onesolutionsweb.com>
2024-01-05 01:11:21 -0400

    post for tweets

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
-use actix_web::{App, HttpServer};
+#[macro_use]
+extern crate diesel;
 
-mod likes;
-mod tweets;
+use actix_web::{web, App, HttpServer};
+use diesel::r2d2::{ConnectionManager, Pool};
+use diesel::PgConnection;
+use dotenv::dotenv;
+use std::env;
 mod constants;
+mod likes;
+mod schema;
+mod tweets;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL env var no encontrada");
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool = Pool::builder()
+        .build(manager)
+        .expect("No pude crear el pool");
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(pool.clone()))
             .service(tweets::get_tweets)
             .service(tweets::create_tweet)
             .service(tweets::get_tweet_by_id)
--- src/tweets.rs
+++ src/tweets.rs
@@ -1,15 +1,29 @@
+use super::schema::tweets;
 use crate::constants::APPLICATION_JSON;
 use actix_web::web::{Data, Path};
 use chrono::{NaiveDateTime, Utc};
 use diesel::r2d2::{ConnectionManager, Pool};
+use diesel::{ExpressionMethods, Insertable, Queryable, RunQueryDsl};
 use uuid::Uuid;
 use diesel::PgConnection;
+use serde::{Deserialize, Serialize};
 use actix_web::{ get, post, HttpResponse};
 
+#[derive(Queryable, Insertable, Serialize, Deserialize)]
 struct Tweet {
     id: Uuid,
     created_at: NaiveDateTime,
     message: String,
+}
+
+impl Tweet {
+    fn new(message: String) -> Self {
+        Self {
+            id: Uuid::new_v4(),
+            created_at: Utc::now().naive_utc(),
+            message,
+        }
+    }
 }
 
 // api/tweets
@@ -24,11 +38,18 @@ pub async fn get_tweets() -> HttpResponse {
 
 #[post("/tweets")]
 pub async fn create_tweet(req_body: String,pool: Data<Pool<ConnectionManager<PgConnection>>>,) -> HttpResponse {
-    // get tweets
-    let new_tweet = "este es mi nuevo tweet";
+    let new_tweet = Tweet::new(req_body);
+    let mut conn = pool
+        .get()
+        .expect(" No pude obtener conexión a la base de datos");
+    diesel::insert_into(tweets::table)
+        .values(&new_tweet)
+        .execute(&mut conn)
+        .expect("Error al insertar tweet");
+
     HttpResponse::Created()
         .content_type(APPLICATION_JSON)
-        .json(new_tweet)
+        .json(&new_tweet)
 }
 
 #[get("/tweets/{id}")]
